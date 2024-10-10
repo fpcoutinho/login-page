b99a51c51c84df4f75dbc0fa21fafc4ff3fae4ec
fpcoutinho <fpcoutinho5@gmail.com>
2024-10-10 18:07:59 -0300

    use font family

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig({
         additionalData: `
           @use 'sass:map';
           @import "sanhaua/system/themes/${SELECTED_THEME}/design-tokens/design-tokens.scss";
+          @import "sanhaua/system/themes/${SELECTED_THEME}/global/global.scss";
           @import "sanhaua/system/themes/${SELECTED_THEME}/web-requirements/web-requirements.scss";
         `
       }
